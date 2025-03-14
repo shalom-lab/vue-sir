e0cbe6af7eb62953da23888bc8f64d602ebaf7ca
Shalom <15121177287@163.com>
2025-03-14 15:08:09 +0800

    init

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
   plugins: [vue()],
+  base: process.env.NODE_ENV === 'production' ? '/vue-sir/' : '/',
   server: {
     port: 3000,
     open: true
